754f64aadab3fbccd75a41157c7d83994a0d75ff
vagrant <vagrant@homestead>
2019-09-04 07:04:19 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,10 @@
 <?php
-
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
 
 class User extends Authenticatable
 {
@@ -36,4 +36,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $table = 'users';
+    
 }
