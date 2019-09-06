d4e006cb9e6193b97352456c12fc792aa3d30ec6
vagrant <vagrant@homestead>
2019-09-06 06:22:24 +0000

    增加 CSRF 验证

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@
 //Route::get('/', 'StaticPagesController@home')->name('home');
 //Route::get('/help', 'StaticPagesController@help')->name('help');
 //Route::get('/about', 'StaticPagesController@about')->name('about');
-//Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup', 'UsersController@create')->name('signup');
 
 
 Route::get('/', 'StaticPagesController@home')->name('home');
@@ -28,4 +28,11 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
 
-//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+
