a16b2189b56da7c276065af303442ce9df42cfe6
YeHaiTian <2821155257@qq.com>
2019-09-11 12:31:10 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,24 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\User;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
-    //
     public function create()
     {
         return view('users.create');
     }
 
-
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
-
 
     public function store(Request $request)
     {
@@ -34,8 +32,8 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
-
 }
